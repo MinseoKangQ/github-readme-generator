297f375a6816210990069ab4e931fd58cbf7988d
MinseoKangQ <kms02171@naver.com>
2024-03-06 11:36:38 +0900

    :bug: 직접 markdown 파일 수정 가능

--- src/Body/ReadmeEditor.jsx
+++ src/Body/ReadmeEditor.jsx
@@ -19,7 +19,7 @@ function ReadmeEditor({ content, onContentChange }) {
       {previewMode ? (
         <div className="markdownPreview" dangerouslySetInnerHTML={{ __html: marked(content) }} />
       ) : (
-        <textarea className="codePreview" value={content} onChange={onContentChange} />
+        <textarea className="codePreview" value={content} onChange={(e) => onContentChange(e.target.value)} />
       )}
     </div>
   );
