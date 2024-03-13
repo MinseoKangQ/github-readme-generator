c563b5f37aa9a60143b2d755c365e4dc0905cc98
MinseoKangQ <kms02171@naver.com>
2024-03-14 00:36:19 +0900

    :sparkles: 코드 copy 기능 추가

--- src/Body/ReadmeEditor.jsx
+++ src/Body/ReadmeEditor.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import PropTypes from 'prop-types'; 
 import { marked } from 'marked';
 import './ReadmeEditor.css';
+import copy from '../assets/copy.svg';
 
 function ReadmeEditor({ content, onContentChange, iconTheme, updateTheme }) {
   const [previewMode, setPreviewMode] = useState(false);
@@ -11,32 +12,39 @@ function ReadmeEditor({ content, onContentChange, iconTheme, updateTheme }) {
     updateTheme(newTheme);
   };
 
-//   return (
-//     <div>
-//       <div className="buttonGroup1">
-//         <button onClick={() => setPreviewMode(false)} className={!previewMode ? 'selected' : ''}>Code</button>
-//         <button onClick={() => setPreviewMode(true)} className={previewMode ? 'selected' : ''}>Preview</button>
-//       </div>
-//       <div className="toggleGroup" onClick={toggleTheme}>
-//         <div className={`toggleBackground ${iconTheme === 'dark' ? 'bg-dark' : 'bg-light'}`}>
-//           <div className={`toggleButton ${iconTheme === 'dark' ? 'toggle-dark' : 'toggle-light'}`}></div>
-//         </div>
-//       </div>
-//       {previewMode ? (
-//         <div className="markdownPreview" dangerouslySetInnerHTML={{ __html: marked(content) }} />
-//       ) : (
-//         <textarea className="codePreview" value={content} onChange={onContentChange} />
-//       )}
-//     </div>
-//   );
-// }
+  const copyMarkdown = () => {
+    navigator.clipboard.writeText(content).then(() => {
+      alert('Success code copy!');
+    }, (err) => {
+      console.error('Error copying text: ', err);
+    });
+  };  
 
 return (
   <div className="editorContainer">
+    {/* <div className="toolbar">
+      <div className="buttonGroup1">
+        <button onClick={() => setPreviewMode(false)} className={!previewMode ? 'selected' : ''}>Code</button>
+        <button onClick={() => setPreviewMode(true)} className={previewMode ? 'selected' : ''}>Preview</button>
+      </div>
+      <div className="rightControls">
+        <div className="copyButton" onClick={copyMarkdown}>
+          <img src={copy} alt="Copy" />
+        </div>
+        <div className="toggleGroup" onClick={toggleTheme}>
+          <div className={`toggleBackground ${iconTheme === 'dark' ? 'bg-dark' : 'bg-light'}`}>
+            <div className={`toggleButton ${iconTheme === 'dark' ? 'toggle-dark' : 'toggle-light'}`}></div>
+          </div>
+        </div>
+      </div>
+    </div> */}
     <div className="toolbar">
       <div className="buttonGroup1">
         <button onClick={() => setPreviewMode(false)} className={!previewMode ? 'selected' : ''}>Code</button>
         <button onClick={() => setPreviewMode(true)} className={previewMode ? 'selected' : ''}>Preview</button>
+        <div className="copyButton" onClick={copyMarkdown}>
+          <img src={copy} alt="Copy" />
+        </div>
       </div>
       <div className="toggleGroup" onClick={toggleTheme}>
         <div className={`toggleBackground ${iconTheme === 'dark' ? 'bg-dark' : 'bg-light'}`}>
